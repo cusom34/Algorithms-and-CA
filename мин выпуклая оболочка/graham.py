422dd29e92b4b0bf5b01d524e3cddea2c67fb78b
Alex Cold <70763567+cusom34@users.noreply.github.com>
2024-06-03 12:34:22 +0300

    Update graham.py

--- мин выпуклая оболочка/graham.py
+++ мин выпуклая оболочка/graham.py
@@ -25,7 +25,7 @@ class Stack:
         self.mass.pop()
 
 def get_cotan(point):
-    return -(point[0]-points[0][0])/(point[1]-points[0][1])
+    return -(point[0] - points[0][0])/(point[1] - points[0][1])
 
 def graham(points):
     s = Stack() #стек, куда пихаем вершины
@@ -38,18 +38,18 @@ def graham(points):
     s.join(sorted_points[0]) #добавляем точку с наименьшим полярным углом
 
     for i in range(1, len(sorted_points)): #идем по сорт. списку
-        u = [s.top()[0] - s.next_to_top()[0], s.top()[1] - s.next_to_top()[1]]
-        v = [sorted_points[i][0] - s.top()[0], sorted_points[i][1] - s.top()[1]]
+        u = [s.top()[0] - s.next_to_top()[0], s.top()[1] - s.next_to_top()[1]]  #первый вектор для условия левого поворота
+        v = [sorted_points[i][0] - s.top()[0], sorted_points[i][1] - s.top()[1]] #второй вектор для условия левого поворота
         while not (u[0] * v[1] - u[1] * v[0] >= 0): #пока не левый поворот на последних 3 точках: удаляем последнюю вершину
             s.pop()
-            u = [s.top()[0] - s.next_to_top()[0], s.top()[1] - s.next_to_top()[1]]
+            u = [s.top()[0] - s.next_to_top()[0], s.top()[1] - s.next_to_top()[1]] #обновляяем векторы
             v = [sorted_points[i][0] - s.top()[0], sorted_points[i][1] - s.top()[1]]
         s.join(sorted_points[i]) #если поворот левый: добавляем точку в стек
 
     return s.get_list()
 
 #points = [[4,2],[-3,-1],[1,2],[-6,5],[5,3],[-2,1], [-1.4,3.2],[5.1,-1.3]]
-points = [[random.randint(0,100),random.randint(0,100)] for i in range(100)]
+points = [[random.randint(0,100), random.randint(0,100)] for i in range(100)]
 
 x = [elem[0] for elem in points]
 y = [elem[1] for elem in points]
@@ -60,7 +60,7 @@ shape_x = [elem[0] for elem in shape]
 shape_y = [elem[1] for elem in shape]
 
 plt.scatter(x,y)
-plt.plot(shape_x + [shape_x[0]], shape_y+ [shape_y[0]])
+plt.plot(shape_x + [shape_x[0]], shape_y + [shape_y[0]])
 plt.show()
 
 
